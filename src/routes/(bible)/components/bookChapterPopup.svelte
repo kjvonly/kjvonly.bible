c5f31ca273955a378ed8ec91ba9da62d7c42862b
man4christ <man4christ.65qa8@simplelogin.com>
2025-01-04 13:41:58 -0500

    book chapter modal filter

--- src/routes/(bible)/components/bookChapterPopup.svelte
+++ src/routes/(bible)/components/bookChapterPopup.svelte
@@ -6,8 +6,17 @@
 	let bookNames: any = $state();
 	let bookIds: any;
 	let bookNamesSorted: any[] = $state([]);
-
+	let filteredBooks: any[] = $state([]);
+	let filterText: string = $state('');
 	let selectedBook: any = $state();
+
+	$effect(() => {
+		filterText;
+
+		filteredBooks = bookNamesSorted.filter((book: { name: string; id: number }) => {
+			return book.name.toLowerCase().includes(filterText);
+		});
+	});
 
 	onMount(async () => {
 		bookNames = await chapterService.getChapter('booknames');
@@ -17,6 +26,7 @@
 
 		for (const i of bookIds) {
 			bookNamesSorted.push({ id: i, name: bookNames['booknamesById'][i] });
+			filteredBooks.push({ id: i, name: bookNames['booknamesById'][i] });
 		}
 	});
 
@@ -39,13 +49,47 @@
 -->
 
 <div class="h-full w-full justify-start justify-items-start overflow-y-scroll">
-	<header class="bg-gray-10 items c sticky top-0 w-full flex-col">
+	<header class="bg-gray-10 items c sticky top-0 w-full flex-col border-b-2 border-white">
 		<div class="flex w-full justify-between p-2 text-gray-500">
-			<span></span>
-			<h1 class="text-lg ">BOOK</h1>
-			<button onclick={() => {showBookChapterPopup = false}} class="m-0 p-0"> Cancel </button>
+			<div class="w-12 h-12">
+				<button onclick={()=> {selectedBook = undefined}} hidden={selectedBook === undefined} aria-label="back to book button">
+					<svg
+						class="h-12 w-12 p-4"
+						version="1.1"
+						width="34.484818"
+						height="58.242714"
+						viewBox="0 0 34.484818 58.242714"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<g id="g8" transform="translate(-40,-34.843996)">
+							<path
+								class="fill-gray-500"
+								style="stroke-width:1.33333"
+								d="M 53,80.35758 C 43.505656,70.810684 40,66.386425 40,63.951131 c 0,-2.445847 3.49976,-6.821123 13.132229,-16.417448 11.374404,-11.331724 13.649954,-13.023883 17,-12.641652 2.904499,0.331396 3.980004,1.235166 4.318418,3.62886 0.353064,2.497337 -1.95028,5.601021 -10.637231,14.333333 L 52.725541,64 63.813416,75.145776 C 72.500367,83.878088 74.803711,86.981772 74.450647,89.479109 74.105181,91.922689 73.066399,92.755693 70,93.048101 66.510733,93.380832 64.340117,91.760465 53,80.35758 Z"
+								id="path170"
+							/>
+						</g>
+					</svg>
+				</button>
+			</div>
+			<div class="flex items-center">
+				{#if selectedBook}
+				<h1 class=" text-center text-lg">CHAPTER</h1>
+				{:else}
+				<h1 class=" text-center text-lg">Book</h1>
+				{/if}
+			</div>
+			<button
+				onclick={() => {
+					showBookChapterPopup = false;
+				}}
+				class="m-0 p-0"
+			>
+				Cancel
+			</button>
 		</div>
 
+		{#if selectedBook === undefined}
 		<div class="p-2">
 			<label class="sr-only" for="name">Name</label>
 			<input
@@ -53,24 +97,28 @@
 				placeholder="Filter Books..."
 				type="text"
 				id="name"
+				bind:value={filterText}
 			/>
 		</div>
+		{/if}
 	</header>
 
 	{#if selectedBook}
 		<div class="grid w-[100%] grid-cols-5 gap-4">
 			{#each new Array(bookNames['maxChapterById'][selectedBook.id]).keys() as ch}
-				<button onclick={() => chapterSelected(ch + 1)} class="row-span-1 p-4 hover:bg-slate-300"
+				<button onclick={() => chapterSelected(ch + 1)} class="hover:bg-gray-10 row-span-1 p-4"
 					>{ch + 1}</button
 				>
 			{/each}
 		</div>
 	{:else}
-		{#each bookNamesSorted as bn}
-			<button
-				onclick={(event) => bookSelected(event, bn)}
-				class="w-full p-4 text-start hover:bg-slate-300">{bn.name}</button
-			>
+		{#each filteredBooks as bn}
+			<div class="w-full">
+				<button
+					onclick={(event) => bookSelected(event, bn)}
+					class="hover:bg-gray-10 w-full p-4 text-start">{bn.name}</button
+				>
+			</div>
 		{/each}
 	{/if}
 </div>
