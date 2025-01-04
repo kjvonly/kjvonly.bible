78d04c9decf92ba458046b8e15882fdfb0adb306
man4christ <man4christ.65qa8@simplelogin.com>
2025-01-03 23:23:10 -0500

    add next prev onclick functions

--- src/routes/(bible)/bible/chapter/+page.svelte
+++ src/routes/(bible)/bible/chapter/+page.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
+	import { bibleNavigationService } from '$lib/services/bible-navigation.service';
 	import Header from '../../components/header.svelte';
 	import Chapter from '../components/chapter.svelte';
 
 	let chapterKey = $state('50_3');
 	let bookName: string = $state('');
 	let bookChapter: string = $state('');
+
+	async function _nextChapter() {
+		chapterKey = bibleNavigationService.next(chapterKey);
+	}
+
+	async function _previousChapter() {
+		chapterKey = bibleNavigationService.previous(chapterKey);
+	}
 </script>
 
 <Header bind:bookName bind:bookChapter bind:chapterKey></Header>
 
-<div class="md:m-12 m-4 flex justify-center">
+<div class="m-4 flex justify-center md:m-12">
 	<div class="max-w-sm md:max-w-lg">
 		<div class="flex flex-wrap justify-start">
 			<Chapter bind:bookName bind:bookChapter bind:chapterKey></Chapter>
@@ -17,12 +26,16 @@
 	</div>
 </div>
 
-<div class="sticky aboslute bottom-28 flex flex-row px-4">
-	<button class="ring-offset-blue-20 h-12 w-12 rounded-full ring-2 ring-slate-200 ring-offset-2 text-gray-500 bg-white"
+<div class="aboslute sticky bottom-28 flex flex-row px-4">
+	<button
+	onclick="{_previousChapter}"
+		class="ring-offset-blue-20 h-12 w-12 rounded-full bg-white text-gray-500 ring-2 ring-slate-200 ring-offset-2"
 		>{'<'}</button
 	>
 	<span class="flex-1"></span>
-	<button class="ring-offset-blue-20 h-12 w-12 rounded-full ring-2 ring-slate-200 ring-offset-2 text-gray-500 bg-white"
+	<button
+	onclick="{_nextChapter}"
+		class="ring-offset-blue-20 h-12 w-12 rounded-full bg-white text-gray-500 ring-2 ring-slate-200 ring-offset-2"
 		>{'>'}</button
 	>
 </div>
