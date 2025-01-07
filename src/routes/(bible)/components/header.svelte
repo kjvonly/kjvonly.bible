78d7b4ac15a1d8ba223d3aa16f64ee630d6cd02f
man4christ <man4christ.65qa8@simplelogin.com>
2025-01-06 23:55:37 -0500

    add color palette

--- src/routes/(bible)/components/header.svelte
+++ src/routes/(bible)/components/header.svelte
@@ -21,10 +21,10 @@
 	}
 </script>
 
-<div class="flex flex-col sticky top-0 z-10 w-[100%] bg-white dark:bg-black max-h-[147.5px]">
+<div class="flex flex-col sticky top-0 z-10 w-[100%] bg-white  dark:bg-black max-h-[147.5px]">
 	<div class="w-full mx-auto max-w-6xl flex-col">
 		<div class="px-2 pt-5">
-			<h1 class="text-3xl font-bold">KJVonly</h1>
+			<h1 class="text-3xl text-primary-500 font-bold">KJVonly</h1>
 		</div>
 
 		<div class="relative flex w-full justify-center pb-2 pt-11 text-base">
