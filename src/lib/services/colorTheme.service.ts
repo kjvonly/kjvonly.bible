2d4742de321808f7bb18d4e8705ff61b043cad42
man4christ <man4christ.65qa8@simplelogin.com>
2025-01-08 23:04:57 -0500

    refactor color theme service

--- src/lib/services/colorTheme.service.ts
+++ src/lib/services/colorTheme.service.ts
@@ -1,35 +1,39 @@
 import { type ChapterSettings, newChapterSettings } from "../../routes/(bible)/bible/models/chapterSettings";
 
 class ColorTheme {
-
-    chapterSettings: ChapterSettings
-    constructor() {
-        this.chapterSettings = newChapterSettings()
-    }
-
     init() {
-        this.chapterSettings = newChapterSettings()
-        let cs = localStorage.getItem('chapterSettings');
-        if (cs != null) {
-            let chapterSettings: ChapterSettings | null = JSON.parse(cs);
-            if (chapterSettings?.colorTheme) {
-                this.setColorTheme(chapterSettings.colorTheme)
-            }
-        }
+
     }
 
-
-
-    setColorTheme(theme: string) {
+    setTheme(theme: string) {
         let html = document.getElementById('kjvonly-html');
-        html?.classList.forEach(   (className: string) => {
-            if (className.includes('color-theme')){
+        html?.classList.forEach((className: string) => {
+            if (className.includes('color-theme')) {
                 html?.classList.remove(className);
             }
         })
+        
+        let cs = this.getChapterSettings()
+        if (!cs) {
+            return
+        }
 
-        html?.classList.add(theme)
-        this.chapterSettings.colorTheme = theme;
+        if (cs.isDarkTheme) {
+            html?.classList.add(`color-theme-dark-${theme}`)
+        } else {
+            html?.classList.add(`color-theme-${theme}`)
+        }
+    }
+
+    getChapterSettings(): ChapterSettings {
+        let cs = localStorage.getItem('chapterSettings');
+        if (cs != null) {
+            let chapterSettings: ChapterSettings | null = JSON.parse(cs);
+            if (chapterSettings) {
+                return chapterSettings
+            }
+        }
+        return newChapterSettings()
     }
 }
 
