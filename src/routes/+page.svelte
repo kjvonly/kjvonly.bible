d4bc8e1f114659fe1c77747b75146a0d7c043676
man4christ <man4christ.65qa8@simplelogin.com>
2025-01-22 22:16:02 -0500

    dynamically add manifest link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { bibleDB } from '$lib/db/bible.db';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
         goto(`${import.meta.env.VITE_BASE_URL}bible/chapter`)
+		let link = document.createElement('link')
+		link.setAttribute('rel', 'manifest')
+		link.setAttribute('href', `${import.meta.env.VITE_BASE_URL}manifest.json`)
+		document.getElementById('kjvonly-head')?.appendChild(link)
 	});
 </script>
 
