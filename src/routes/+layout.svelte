a1965f8bc43df8c8d4d5d91f9d59aac2e2e9cb29
man4christ <man4christ.65qa8@simplelogin.com>
2025-02-05 14:58:06 -0500

    remove (bible) route. we handle all navigation thorugh our pane service. no need to route anywhere else

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,16 @@
 	import { onMount } from 'svelte';
 	import '../app.css';
 	import { bibleDB } from '$lib/db/bible.db';
-	
+	import Container from '$lib/components/container.svelte';
+
 	onMount(async () => {
 		/* This pulls the chapter and strongs data from api and stores in indexdb for offline use. */
 		bibleDB.init();
 	});
-	
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Container>
+	{@render children?.()}
+</Container>
