be5ff41e58fd0c3e7487d3d5f654872dfdfd5a88
man4christ <man4christ.65qa8@simplelogin.com>
2025-02-08 18:54:37 -0500

    wrap building refs in try catch in case a ref doesnt exist for some reason

--- src/lib/modules/refs/verses-refs/verseRefsContainer.svelte
+++ src/lib/modules/refs/verses-refs/verseRefsContainer.svelte
@@ -30,24 +30,28 @@
 		vrefs.push(vref);
 
 		verseRefs.forEach(async (ref: string) => {
-			let index = ref.lastIndexOf('/');
-			let ckey = ref.substring(0, index).replaceAll('/', '_');
-			let cnumber = ckey.split('_')[1];
-			let vnumber = ref.substring(index + 1, ref.length);
-			let data = await bibleDB.getValue('chapters', ckey);
-			let bname = data['bookName'];
-			let v = data['verseMap'][vnumber];
-			let vNoVn = v.substring(0, v.length);
+			try {
+				let index = ref.lastIndexOf('/');
+				let ckey = ref.substring(0, index).replaceAll('/', '_');
+				let cnumber = ckey.split('_')[1];
+				let vnumber = ref.substring(index + 1, ref.length);
+				let data = await bibleDB.getValue('chapters', ckey);
+				let bname = data['bookName'];
+				let v = data['verseMap'][vnumber];
+				let vNoVn = v.substring(0, v.length);
 
-			let vref = {
-				ref: ref,
-				bookName: bname,
-				chapter: cnumber,
-				vnumber: vnumber,
-				text: vNoVn
-			};
-			vrefs.push(vref);
-			console.log(vref);
+				let vref = {
+					ref: ref,
+					bookName: bname,
+					chapter: cnumber,
+					vnumber: vnumber,
+					text: vNoVn
+				};
+				vrefs.push(vref);
+				console.log(vref);
+			} catch (ex) {
+				console.log(`error fetching ref ${ref}`);
+			}
 		});
 
 		verseRefs2.push(vrefs);
@@ -55,9 +59,8 @@
 </script>
 
 {#snippet vrefSnippet(vref: any)}
-	
 	<p class="px-4 py-2 text-left hover:bg-primary-100">
-        <span class="font-bold">{vref.bookName} {vref.chapter}:{vref.vnumber}</span><br/>
+		<span class="font-bold">{vref.bookName} {vref.chapter}:{vref.vnumber}</span><br />
 		{#each vref.text.trim().split(' ') as w}
 			<span class="inline-block">{w}</span>&nbsp;
 		{/each}
@@ -66,17 +69,14 @@
 
 <div>
 	<div class="py-4">
-
-        
-		
 		<div class="py-4">
 			{#if verseRefs2.length > 0}
-            <h1 class="py-4 font-bold underline underline-offset-8">Verse</h1>
+				<h1 class="py-4 font-bold underline underline-offset-8">Verse</h1>
 				{@const vref = verseRefs2[verseRefs2.length - 1][0]}
 
 				{@render vrefSnippet(vref)}
-                
-                <h1 class="py-4 font-bold underline underline-offset-8">Verse References</h1>
+
+				<h1 class="py-4 font-bold underline underline-offset-8">Verse References</h1>
 				{#each verseRefs2[verseRefs2.length - 1].slice(1, verseRefs2[verseRefs2.length - 1].length) as vref, idx}
 					{@render vrefSnippet(vref)}
 				{/each}
