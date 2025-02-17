4b81e239a2cde52b497bbe5a0b4190c72ca65bc6
man4christ <man4christ.65qa8@simplelogin.com>
2025-02-16 19:05:46 -0500

    bundle quill css in with build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import '../app.css';
 	import { bibleDB } from '$lib/db/bible.db';
 	import Container from '$lib/components/container.svelte';
+	import '../../node_modules/quill/dist/quill.snow.css'
 
 	onMount(async () => {
 		/* This pulls the chapter and strongs data from api and stores in indexdb for offline use. */
