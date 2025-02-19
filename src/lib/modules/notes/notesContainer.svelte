cdcb9147b9d3b10e680f092c95631e3c38f62ddd
man4christ <man4christ.65qa8@simplelogin.com>
2025-02-18 22:44:45 -0500

    refactor note list to snippets

--- src/lib/modules/notes/notesContainer.svelte
+++ src/lib/modules/notes/notesContainer.svelte
@@ -303,7 +303,7 @@
 	});
 </script>
 
-<!-- END NOTE SNIPPETS -->
+<!-- START NOTE SNIPPETS -->
 {#snippet noteHeaderSnippet()}
 	<header
 		bind:clientHeight={headerHeight}
@@ -498,6 +498,176 @@
 	</div>
 {/snippet}
 <!-- END NOTE SNIPPETS -->
+
+<!-- START NOTE LIST SNIPPETS -->
+{#snippet noteListHeader()}
+	<header
+		bind:clientHeight={headerHeight}
+		class=" flex w-full max-w-lg flex-row items-center justify-between bg-neutral-100 text-neutral-700"
+	>
+		<button
+			aria-label="close"
+			onclick={() => {
+				onAdd();
+			}}
+			class="h-12 w-12 px-2 pt-2 text-neutral-700"
+		>
+			<svg
+				class="p-0.5"
+				version="1.1"
+				width="100%"
+				height="100%"
+				viewBox="0 0 105.50072 106.78786"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<g id="g8" transform="translate(-11.214067,-10.602166)">
+					<path
+						class="fill-neutral-700"
+						style="stroke-width:4.20363;stroke-linejoin:round"
+						d="M 63.952348,10.627557 A 52.737736,53.368481 0 0 0 11.214067,63.996697 52.737736,53.368481 0 0 0 63.952348,117.36388 52.737736,53.368481 0 0 0 116.68868,63.996697 52.737736,53.368481 0 0 0 63.952348,10.627557 Z m -4.40625,34.925781 h 8.884766 v 14.335937 h 12.917969 v 8.138672 H 68.430864 V 82.438103 H 59.546098 V 68.027947 H 46.553911 v -8.138672 h 12.992187 z"
+					/>
+				</g>
+			</svg>
+		</button>
+		<!-- svelte-ignore a11y_click_events_have_key_events -->
+		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+		<p
+			onclick={() => {
+				showNoteListActions = !showNoteListActions;
+			}}
+			class="hover:cursor-pointer"
+		>
+			<span class="inline-block font-bold">Note</span>
+			<button aria-label="chevron down" class="h-4 w-4">
+				<svg
+					width="100%"
+					height="100%"
+					viewBox="0 0 25.4 14.098638"
+					version="1.1"
+					xml:space="preserve"
+					xmlns="http://www.w3.org/2000/svg"
+					><g transform="translate(-53.644677,-127.79211)"
+						><path
+							class="fill-neutral-700"
+							style="stroke-width:0.352778"
+							d="m 59.906487,137.65245 -6.26181,-4.21622 v -2.82206 -2.82206 l 6.35,4.24282 6.35,4.24283 6.35,-4.24283 6.35,-4.24282 v 2.82222 2.82222 l -6.3429,4.23808 c -3.48859,2.33094 -6.38578,4.22817 -6.43819,4.21606 -0.0524,-0.0121 -2.91311,-1.91931 -6.3571,-4.23824 z"
+							id="path179"
+						/></g
+					></svg
+				>
+			</button>
+		</p>
+		<button
+			aria-label="close"
+			onclick={() => {
+				if (allNotes) {
+					paneService.onDeletePane(paneService.rootPane, mode.paneId);
+				} else {
+					mode.notePopup.show = false;
+				}
+			}}
+			class="h-12 w-12 px-2 pt-2 text-neutral-700"
+		>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100%" height="100%">
+				<path
+					class="fill-neutral-700"
+					d="M12,2C6.47,2,2,6.47,2,12s4.47,10,10,10s10-4.47,10-10S17.53,2,12,2z M17,15.59L15.59,17L12,13.41L8.41,17L7,15.59 L10.59,12L7,8.41L8.41,7L12,10.59L15.59,7L17,8.41L13.41,12L17,15.59z"
+				/>
+			</svg>
+		</button>
+	</header>
+{/snippet}
+
+{#snippet noteListFilter()}
+	<div class="flex flex-col justify-start px-2">
+		<label
+			for="tags"
+			class="relative block overflow-hidden border-b border-neutral-200 bg-transparent pt-3 focus-within:border-supporta-600"
+		>
+			<div class="flex items-center">
+				<input
+					type="tags"
+					id="tags"
+					placeholder="Search Notes..."
+					bind:value={filterInput}
+					oninput={onFilterInputChanged}
+					class="focus:outline-hidden focus:ring-none peer h-8 w-full border-none bg-transparent p-0 outline-none focus:border-transparent"
+				/>
+			</div>
+		</label>
+		<div>
+			<fieldset>
+				<legend class="sr-only">Checkboxes</legend>
+				{#each filterParams as fp}
+					<div class="space-y-2">
+						<label for="Option1" class="flex cursor-pointer items-start gap-4">
+							<div class="flex items-center">
+								&#8203;
+								<input
+									bind:checked={fp.checked}
+									type="checkbox"
+									class="size-4 rounded-sm border-neutral-200 accent-supporta-300"
+									id="Option1"
+								/>
+							</div>
+
+							<div>
+								<strong class="capitalize text-neutral-500">
+									{fp.option}
+								</strong>
+							</div>
+						</label>
+					</div>
+				{/each}
+			</fieldset>
+		</div>
+	</div>
+{/snippet}
+
+{#snippet noteListSnippet()}
+	{#each noteKeys as nk}
+		<button
+			onclick={() => {
+				onSelectedNote(nk);
+			}}
+			class="flex w-full flex-nowrap p-2 text-left hover:bg-neutral-100"
+		>
+			<div class="flex flex-col">
+				<span>{notes[nk].title}</span>
+				<span class="text-neutral-400"
+					>{new Date(notes[nk].modified).toLocaleDateString()}
+					{new Date(notes[nk].modified).toLocaleTimeString()}</span
+				>
+				<div class="flex flex-row justify-start space-x-2 pt-2">
+					{#each notes[nk].tags as tk}
+						<span
+							class="inline-flex h-8 items-center justify-center rounded-full border border-supporta-500 px-2.5 py-0.5 text-supporta-700"
+						>
+							<p class="whitespace-nowrap text-sm">{tk.tag}</p>
+						</span>
+					{/each}
+				</div>
+			</div>
+		</button>
+	{/each}
+{/snippet}
+
+{#snippet noteListActionsSnippet()}
+	<div
+		class="flex h-full w-full max-w-lg flex-col items-start justify-start border border-neutral-100"
+	>
+		{#each Object.keys(noteListActions) as na}
+			<button
+				class="w-full py-4 ps-2 text-left capitalize hover:bg-primary-50"
+				aria-label="note action button"
+				onclick={() => noteListActions[na]()}
+			>
+				{na}
+			</button>
+		{/each}
+	</div>
+{/snippet}
+
 <div
 	bind:clientHeight
 	style={containerHeight}
@@ -521,174 +691,22 @@
 			</div>
 		{/if}
 	{:else}
-		<header
-			bind:clientHeight={headerHeight}
-			class=" flex w-full max-w-lg flex-row items-center justify-between bg-neutral-100 text-neutral-700"
-		>
-			<button
-				aria-label="close"
-				onclick={() => {
-					onAdd();
-				}}
-				class="h-12 w-12 px-2 pt-2 text-neutral-700"
-			>
-				<svg
-					class="p-0.5"
-					version="1.1"
-					width="100%"
-					height="100%"
-					viewBox="0 0 105.50072 106.78786"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<g id="g8" transform="translate(-11.214067,-10.602166)">
-						<path
-							class="fill-neutral-700"
-							style="stroke-width:4.20363;stroke-linejoin:round"
-							d="M 63.952348,10.627557 A 52.737736,53.368481 0 0 0 11.214067,63.996697 52.737736,53.368481 0 0 0 63.952348,117.36388 52.737736,53.368481 0 0 0 116.68868,63.996697 52.737736,53.368481 0 0 0 63.952348,10.627557 Z m -4.40625,34.925781 h 8.884766 v 14.335937 h 12.917969 v 8.138672 H 68.430864 V 82.438103 H 59.546098 V 68.027947 H 46.553911 v -8.138672 h 12.992187 z"
-						/>
-					</g>
-				</svg>
-			</button>
-			<!-- svelte-ignore a11y_click_events_have_key_events -->
-			<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-			<p
-				onclick={() => {
-					showNoteListActions = !showNoteListActions;
-				}}
-				class="hover:cursor-pointer"
-			>
-				<span class="inline-block font-bold">Note</span>
-				<button aria-label="chevron down" class="h-4 w-4">
-					<svg
-						width="100%"
-						height="100%"
-						viewBox="0 0 25.4 14.098638"
-						version="1.1"
-						xml:space="preserve"
-						xmlns="http://www.w3.org/2000/svg"
-						><g transform="translate(-53.644677,-127.79211)"
-							><path
-								class="fill-neutral-700"
-								style="stroke-width:0.352778"
-								d="m 59.906487,137.65245 -6.26181,-4.21622 v -2.82206 -2.82206 l 6.35,4.24282 6.35,4.24283 6.35,-4.24283 6.35,-4.24282 v 2.82222 2.82222 l -6.3429,4.23808 c -3.48859,2.33094 -6.38578,4.22817 -6.43819,4.21606 -0.0524,-0.0121 -2.91311,-1.91931 -6.3571,-4.23824 z"
-								id="path179"
-							/></g
-						></svg
-					>
-				</button>
-			</p>
-			<button
-				aria-label="close"
-				onclick={() => {
-					if (allNotes) {
-						paneService.onDeletePane(paneService.rootPane, mode.paneId);
-					} else {
-						mode.notePopup.show = false;
-					}
-				}}
-				class="h-12 w-12 px-2 pt-2 text-neutral-700"
-			>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100%" height="100%">
-					<path
-						class="fill-neutral-700"
-						d="M12,2C6.47,2,2,6.47,2,12s4.47,10,10,10s10-4.47,10-10S17.53,2,12,2z M17,15.59L15.59,17L12,13.41L8.41,17L7,15.59 L10.59,12L7,8.41L8.41,7L12,10.59L15.59,7L17,8.41L13.41,12L17,15.59z"
-					/>
-				</svg>
-			</button>
-		</header>
-
+		{@render noteListHeader()}
 		{#if !showNoteListActions}
 			<div
 				class="flex h-full w-full max-w-lg flex-col overflow-hidden overflow-y-scroll border border-neutral-100"
 			>
 				{#if showNoteListFilter}
-					<div class="flex flex-col justify-start px-2">
-						<label
-							for="tags"
-							class="relative block overflow-hidden border-b border-neutral-200 bg-transparent pt-3 focus-within:border-supporta-600"
-						>
-							<div class="flex items-center">
-								<input
-									type="tags"
-									id="tags"
-									placeholder="Search Notes..."
-									bind:value={filterInput}
-									oninput={onFilterInputChanged}
-									class="focus:outline-hidden focus:ring-none peer h-8 w-full border-none bg-transparent p-0 outline-none focus:border-transparent"
-								/>
-							</div>
-						</label>
-						<div>
-							<fieldset>
-								<legend class="sr-only">Checkboxes</legend>
-								{#each filterParams as fp}
-									<div class="space-y-2">
-										<label for="Option1" class="flex cursor-pointer items-start gap-4">
-											<div class="flex items-center">
-												&#8203;
-												<input
-													bind:checked={fp.checked}
-													type="checkbox"
-													class="size-4 rounded-sm border-neutral-200 accent-supporta-300"
-													id="Option1"
-												/>
-											</div>
-
-											<div>
-												<strong class="capitalize text-neutral-500">
-													{fp.option}
-												</strong>
-											</div>
-										</label>
-									</div>
-								{/each}
-							</fieldset>
-						</div>
-					</div>
+					{@render noteListFilter()}
 				{/if}
-				{#each noteKeys as nk}
-					<button
-						onclick={() => {
-							onSelectedNote(nk);
-						}}
-						class="flex w-full flex-nowrap p-2 text-left hover:bg-neutral-100"
-					>
-						<div class="flex flex-col">
-							<span>{notes[nk].title}</span>
-							<span class="text-neutral-400"
-								>{new Date(notes[nk].modified).toLocaleDateString()}
-								{new Date(notes[nk].modified).toLocaleTimeString()}</span
-							>
-							<div class="flex flex-row justify-start space-x-2 pt-2">
-								{#each notes[nk].tags as tk}
-									<span
-										class="inline-flex h-8 items-center justify-center rounded-full border border-supporta-500 px-2.5 py-0.5 text-supporta-700"
-									>
-										<p class="whitespace-nowrap text-sm">{tk.tag}</p>
-									</span>
-								{/each}
-							</div>
-						</div>
-					</button>
-				{/each}
+				{@render noteListSnippet()}
 			</div>
 		{/if}
 		{#if showNoteListActions}
-			<div
-				class="flex h-full w-full max-w-lg flex-col items-start justify-start border border-neutral-100"
-			>
-				{#each Object.keys(noteListActions) as na}
-					<button
-						class="w-full py-4 ps-2 text-left capitalize hover:bg-primary-50"
-						aria-label="note action button"
-						onclick={() => noteListActions[na]()}
-					>
-						{na}
-					</button>
-				{/each}
-			</div>
+			{@render noteListActionsSnippet()}
 		{/if}
 	{/if}
+	<!-- END NOTE LIST SNIPPETS -->
 
 	<!-- keep the editor in the dom the while notes container is open. toggle the hidden params. Otherwise we'd need to keep creating this. -->
 	<div
