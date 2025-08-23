e34613f4bdef5f5b93997ecb9a2d4b4e3a872447
man4christ <man4christ.65qa8@simplelogin.com>
2025-08-23 00:28:57 -0700

    update tests

--- api/frontends/bible/src/lib/api/chapter.api.spec.ts
+++ api/frontends/bible/src/lib/api/chapter.api.spec.ts
@@ -16,38 +16,86 @@ describe('chapterService', () => {
     bibleService = Object()
     bibleService.putValue = vi.fn()
 
-
     chapterService = new ChapterService(api, bibleService)
   })
 
-  it('should post annot incrementing the vesion to 1', async () => {
-    let data = {
-      id: "50_3",
-      annots: {},
-      version: 0,
-  
-    }
 
-    let expectedData = JSON.parse(JSON.stringify(data))
-    expectedData.version = 1
-    expectedData.userID = '45b5fbd3-755f-4379-8f07-a58d4a30fa2f'
-    expectedData.dateCreated =  1755929876
-    expectedData.dateUpdated = 1755929876
+  let tt = [
+    { startVersion: 0, endVersion: 1 },
+  ]
+  tt.forEach((t) => {
+    it(`should post annot incrementing the vesion from ${t.startVersion} to ${t.endVersion}`, async () => {
+      let data = {
+        id: "50_3",
+        annots: {},
+        version: t.startVersion,
+      }
 
-    const mockResponse = {
-      json: () => Promise.resolve(expectedData),
-      ok: true
-    };
+      let expectedData = {
+        id: "50_3",
+        annots: {},
+        version: t.endVersion,
+        "userID": '45b5fbd3-755f-4379-8f07-a58d4a30fa2f',
+        "dateCreated": 1755929876,
+        "dateUpdated": 1755929876
+      }
+
+      const mockResponse = {
+        json: () => Promise.resolve(expectedData),
+        ok: true
+      };
+
+      api.postapi.mockResolvedValueOnce(mockResponse)
+
+      let ua = await chapterService.putAnnotations(data)
+
+      expect(ua).toEqual(expectedData)
+      expect(api.postapi).toBeCalledTimes(1)
+      expect(api.postapi).toBeCalledWith('/annots', data)
+      expect(bibleService.putValue).toBeCalledTimes(1)
+      expect(bibleService.putValue).toBeCalledWith('annotations', expectedData)
+    })
+  })
 
 
-    api.postapi.mockResolvedValueOnce(mockResponse)
+ tt = [
+    
+    { startVersion: 1, endVersion: 2 },
+    { startVersion: 3, endVersion: 4 }
+  ]
+  tt.forEach((t) => {
+    it(`should post annot incrementing the vesion from ${t.startVersion} to ${t.endVersion}`, async () => {
+      let data = {
+        id: "50_3",
+        annots: {},
+        version: t.startVersion,
+      }
 
-    await chapterService.putAnnotations(data)
+      let expectedData = {
+        id: "50_3",
+        annots: {},
+        version: t.endVersion,
+        "userID": '45b5fbd3-755f-4379-8f07-a58d4a30fa2f',
+        "dateCreated": 1755929876,
+        "dateUpdated": 1755929876
+      }
 
-    expect(api.postapi).toBeCalledTimes(1)
-    expect(api.postapi).toBeCalledWith('/annots', data)
-    expect(bibleService.putValue).toBeCalledTimes(1)
-    expect(bibleService.putValue).toBeCalledWith('annotations', expectedData)
+      const mockResponse = {
+        json: () => Promise.resolve(expectedData),
+        ok: true
+      };
+
+
+      api.updateapi.mockResolvedValueOnce(mockResponse)
+
+      let ua = await chapterService.putAnnotations(data)
+
+      expect(ua).toEqual(expectedData)
+      expect(api.updateapi).toBeCalledTimes(1)
+      expect(api.updateapi).toBeCalledWith(`/annots/${data.id}`, data)
+      expect(bibleService.putValue).toBeCalledTimes(1)
+      expect(bibleService.putValue).toBeCalledWith('annotations', expectedData)
+    })
   })
 
 })
