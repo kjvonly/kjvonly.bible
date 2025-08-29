667132b02daf5dac81e5fb63eada1fc15d29a1f5
man4christ <man4christ.65qa8@simplelogin.com>
2025-08-28 20:36:02 -0700

    refactor sync bible data funcs

--- api/frontends/bible/src/lib/workers/kjvsync.worker.ts
+++ api/frontends/bible/src/lib/workers/kjvsync.worker.ts
@@ -1,9 +1,21 @@
-import { ChapterService } from "$lib/api/chapters.api";
-import { BibleDB, ANNOTATIONS, UNSYNCED_ANNOTATIONS, NOTES, UNSYNCED_NOTES } from "$lib/db/bible.db";
 import { BibleService } from "$lib/db/bible.service";
+import { ChapterService } from "$lib/api/chapters.api";
+import {
+    BibleDB,
+    ANNOTATIONS,
+    UNSYNCED_ANNOTATIONS,
+    NOTES,
+    UNSYNCED_NOTES,
+    CHAPTERS,
+    BOOKNAMES
+} from "$lib/db/bible.db";
+
+
 const API_URL = `${import.meta.env.VITE_API_URL}`
 const BASE_URL = `${import.meta.env.VITE_BASE_URL}/`
 
+
+// can't access local storage from web worker. Need to duplicate this simple api class.
 let token: any = undefined
 class Api {
     async getapi(path: string): Promise<Response> {
@@ -22,7 +34,7 @@ class Api {
         );
     }
 
-        // Temp function while combinding frontend to backend
+    // Temp function while combinding frontend to backend
     async postapi(path: string, data: any): Promise<Response> {
         let headers: any = {
             'Content-Type': 'application/json'
@@ -37,10 +49,9 @@ class Api {
             headers: headers,
             body: JSON.stringify(data)
         });
-
     }
 
-      async updateapi(path: string, data: any): Promise<Response> {
+    async updateapi(path: string, data: any): Promise<Response> {
         let headers: any = {
             'Content-Type': 'application/json'
         }
@@ -54,120 +65,111 @@ class Api {
             headers: headers,
             body: JSON.stringify(data)
         });
-
     }
-
 }
 
-let db: BibleDB | undefined = undefined
+
+let db = await BibleDB.CreateAsync()
 let api: Api = new Api()
 
-async function sync(data: any) {
-    if (db === undefined) {
-        db = await BibleDB.CreateAsync()
-    }
-
+async function syncAnnotsAndNotesFromServer(data: any) {
     token = data.token
     let chapterService = new ChapterService(api, new BibleService(db))
-    await chapterService.sync('/annots',  UNSYNCED_ANNOTATIONS, ANNOTATIONS)
+
+    // ----------------- SYNC ANNOTS ------------------------------------------
+    await chapterService.sync('/annots', UNSYNCED_ANNOTATIONS, ANNOTATIONS)
     postMessage({ id: 'annotations' })
 
-    await chapterService.sync('/notes',  UNSYNCED_NOTES, NOTES)
+    // ----------------- SYNC NOTES -------------------------------------------
+    await chapterService.sync('/notes', UNSYNCED_NOTES, NOTES)
     postMessage({ id: 'notes' })
 }
 
 
 // SYNC BIBLE DATA
+const BIBLE_DATA_HEADERS = new Headers();
+BIBLE_DATA_HEADERS.append('Content-Type', 'application/json');
+BIBLE_DATA_HEADERS.append('Transfer-Encoding', 'gzip');
 
+async function fetchAndStoreAllBibleChapters() {
+    try {
+        let res = await fetch(`${BASE_URL}data/json.gz/all.json`, {
+            headers: BIBLE_DATA_HEADERS
+        })
 
-const myHeaders = new Headers();
-myHeaders.append('Content-Type', 'application/json');
-myHeaders.append('Transfer-Encoding', 'gzip');
-
-
-async function onChapters() {
-	let db = await BibleDB.CreateAsync()
-	fetch(`${BASE_URL}data/json.gz/all.json`, {
-		headers: myHeaders
-	}).then((res) => {
-		res.json().then((json) => {
-			let myMap = new Map<string, any>(Object.entries(json));
-			myMap.forEach((value: any, key: string) => {
-				value['id'] = key;
-				db.putValue('chapters', value);
-			});
-		});
-	}).catch((err) => {
-		console.log(`error: ${err}`)
-	});
-}	
-
-async function onBooknames() {
-	let db = await BibleDB.CreateAsync()
-	fetch(`${BASE_URL}data/json.gz/booknames.json`, {
-		headers: myHeaders
-	}).then((res) => {
-
-		res.json().then((json) => {
-			json['id'] = 'booknames';
-			db.putValue('booknames', json);
-		});
-	}).catch((err) => {
-		console.log(`error: ${err}`)
-	});;
+        let json = await res.json()
+        let chapters = new Map<string, any>(Object.entries(json));
+        chapters.forEach((chapter: any, chapterKey: string) => {
+            chapter['id'] = chapterKey;
+            db.putValue(CHAPTERS, chapter);
+        });
+    }
+    catch (err) {
+        console.log(`error: ${err}`)
+    }
 }
 
-async function onSearch() {
-	let db = await BibleDB.CreateAsync()
-	fetch(`${BASE_URL}data/json.gz/bibleindex.json`, {
-		headers: myHeaders
-	}).then((res) => {
-
-		res.json().then((json) => {
-			db.putValue('search', json);
-		});
-	}).catch((err) => {
-		console.log(`error: ${err}`)
-	});;
+async function fetchAndStoreBooknames() {
+    try {
+        let res = await fetch(`${BASE_URL}data/json.gz/booknames.json`, {
+            headers: BIBLE_DATA_HEADERS
+        })
+        let json = await res.json()
+        json['id'] = BOOKNAMES;
+        db.putValue(BOOKNAMES, json);
+    } catch (err) {
+        console.log(`error: ${err}`)
+    }
 }
 
-async function onStrongs() {
-	let db = await BibleDB.CreateAsync()
-	fetch(`${BASE_URL}data/strongs.json.gz/all.json`, {
-		headers: myHeaders
-	}).then((res) => {
-		res.json().then((json) => {
-			let myMap = new Map<string, any>(Object.entries(json));
-			myMap.forEach((value: any, key: string) => {
-				value['id'] = key;
-				db.putValue('strongs', value);
-			});
-		});
-	}).catch((err) => {
-		console.log(`error: ${err}`)
-	});
+async function fetchAndStoreSearchBibleIndex() {
+    try {
+        let res = await fetch(`${BASE_URL}data/json.gz/bibleindex.json`, {
+            headers: BIBLE_DATA_HEADERS
+        })
+        let json = await res.json()
+        db.putValue('search', json);
+
+    } catch (err) {
+        console.log(`error: ${err}`)
+    }
+}
+
+async function fetchAndStoreStrongsDefs() {
+    try {
+        let res = await fetch(`${BASE_URL}data/strongs.json.gz/all.json`, {
+            headers: BIBLE_DATA_HEADERS
+        })
+        let json = await res.json()
+        let myMap = new Map<string, any>(Object.entries(json));
+        myMap.forEach((value: any, key: string) => {
+            value['id'] = key;
+            db.putValue('strongs', value);
+        });
+
+    } catch (err) {
+        console.log(`error: ${err}`)
+    }
 }
 
 onmessage = async (e) => {
     switch (e.data.action) {
         case 'init':
-            await sync(e.data)
+            await syncAnnotsAndNotesFromServer(e.data)
             break;
         case 'sync':
-            await sync(e.data)
+            await syncAnnotsAndNotesFromServer(e.data)
             break;
         case 'chapters':
-			onChapters();
-			break;
-		case 'booknames':
-			onBooknames();
-			break;
-		case 'strongs':
-			onStrongs();
-			break;
-		case 'search':
-			onSearch();
+            fetchAndStoreAllBibleChapters();
+            break;
+        case 'booknames':
+            fetchAndStoreBooknames();
+            break;
+        case 'strongs':
+            fetchAndStoreStrongsDefs();
+            break;
+        case 'search':
+            fetchAndStoreSearchBibleIndex();
     }
 }
-
-export { };
