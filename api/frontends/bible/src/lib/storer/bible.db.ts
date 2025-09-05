af0783402a380bb72f7035dba66f8dcafec1b687
man4christ <man4christ.65qa8@simplelogin.com>
2025-09-04 21:45:17 -0700

    add plan indexeddb tables

--- api/frontends/bible/src/lib/storer/bible.db.ts
+++ api/frontends/bible/src/lib/storer/bible.db.ts
@@ -1,25 +1,46 @@
 import IndexedDB from './idb.db';
 
+const DB_VERSION = 7
+
 export const enum STORES {
 	CHAPTERS,
 	BOOKNAMES,
 	STRONGS,
-	ANNOTATIONS,
-	NOTES,
 	SEARCH,
+	ANNOTATIONS,
 	UNSYNCED_ANNOTATIONS,
-	UNSYNCED_NOTES
+	NOTES,
+	UNSYNCED_NOTES,
+	PLANS,
+	UNSYNCED_PLANS,
+	PLAN_SUBSCRIPTIONS,
+	UNSYNCED_PLAN_SUBSCRIPTIONS,
+	PLAN_TRACKER,
+	UNSYNCED_PLAN_TRACKER,
 }
 
 export const DB_NAME = 'bible';
+
 export const CHAPTERS = 'chapters';
 export const BOOKNAMES = 'booknames';
 export const STRONGS = 'strongs';
-export const ANNOTATIONS = 'annotations';
-export const NOTES = 'notes';
 export const SEARCH = 'search';
+
+export const ANNOTATIONS = 'annotations';
 export const UNSYNCED_ANNOTATIONS = 'unsynced_annotations';
+
+export const NOTES = 'notes';
 export const UNSYNCED_NOTES = 'unsynced_notes';
+
+export const PLANS = 'plans';
+export const UNSYNCED_PLANS = 'unsynced_plans';
+
+export const PLAN_SUBSCRIPTIONS = 'plan_subscription';
+export const UNSYNCED_PLAN_SUBSCRIPTIONS = 'unsynced_plan_subscription';
+
+export const PLAN_TRACKER = 'plan_tracker';
+export const UNSYNCED_PLAN_TRACKER = 'unsynced_plan_tracker';
+
 
 export class BibleDB extends IndexedDB {
 	constructor() {
@@ -32,12 +53,23 @@ export class BibleDB extends IndexedDB {
 			CHAPTERS,
 			BOOKNAMES,
 			STRONGS,
-			ANNOTATIONS,
-			NOTES,
 			SEARCH,
+
+			ANNOTATIONS,
 			UNSYNCED_ANNOTATIONS,
-			UNSYNCED_NOTES
-		]);
+			
+			NOTES,
+			UNSYNCED_NOTES,
+
+			PLANS,
+			UNSYNCED_PLANS,
+
+			PLAN_SUBSCRIPTIONS,
+			UNSYNCED_PLAN_SUBSCRIPTIONS,
+
+			PLAN_TRACKER,
+			UNSYNCED_PLAN_TRACKER
+		], DB_VERSION);
 		return this.instance;
 	}
 }
