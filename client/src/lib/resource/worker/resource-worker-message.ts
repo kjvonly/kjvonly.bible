b0a7e498b70c6be910d9dbc412fdeae7717cff92
man4christ <man4christ.65qa8@simplelogin.com>
2026-09-01 20:55:49 -0400

    add resource child worker process types to worker message

--- client/src/lib/resource/worker/resource-worker-message.ts
+++ client/src/lib/resource/worker/resource-worker-message.ts
@@ -297,3 +297,53 @@ function deserializeResourceWorkerInstallOutcome(
 			)
 	};
 }
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Resource Coordinator → Child Resource Worker
+
+export interface ResourceChildWorkerProcessRequest {
+	readonly type:
+		'process';
+
+	readonly requestId:
+		string;
+
+	readonly requested:
+		PublishedResourceReference;
+
+	readonly representation:
+		ResourceRepresentation;
+}
+
+export type ResourceChildWorkerRequest =
+	ResourceChildWorkerProcessRequest;
+
+///////////////////////////////////////////////////////////////////////////////
+// Child Resource Worker → Resource Coordinator
+
+export interface ResourceChildWorkerProcessResult {
+	readonly type:
+		'process-result';
+
+	readonly requestId:
+		string;
+
+	readonly result:
+		ResourceWorkerInstallResult;
+}
+
+export interface ResourceChildWorkerProcessError {
+	readonly type:
+		'process-error';
+
+	readonly requestId:
+		string;
+
+	readonly error:
+		ResourceWorkerError;
+}
+
+export type ResourceChildWorkerMessage =
+	| ResourceChildWorkerProcessResult
+	| ResourceChildWorkerProcessError;
