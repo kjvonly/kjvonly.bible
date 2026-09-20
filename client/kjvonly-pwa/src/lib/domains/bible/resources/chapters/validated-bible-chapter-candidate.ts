c7859bdfe033a54da06912be2996940e2043f67d
man4christ <man4christ.65qa8@simplelogin.com>
2026-09-20 14:24:35 -0400

    bible chapter resource relative imports

--- client/kjvonly-pwa/src/lib/domains/bible/resources/chapters/validated-bible-chapter-candidate.ts
+++ client/kjvonly-pwa/src/lib/domains/bible/resources/chapters/validated-bible-chapter-candidate.ts
@@ -1,6 +1,6 @@
 import type {
 	ChapterContent
-} from '$lib/domains/bible/models/bible.model';
+} from '../../models/bible.model';
 
 export interface ValidatedBibleChapterCandidate {
 	readonly version:
