ee7d7e9eedace13635d8b6f3f9686623c014f633
man4christ <man4christ.65qa8@simplelogin.com>
2026-09-20 20:36:42 -0400

    add night colorblind

--- client/kjvonly-pwa/src/lib/application/modules/settings/colorTheme.svelte
+++ client/kjvonly-pwa/src/lib/application/modules/settings/colorTheme.svelte
@@ -17,6 +17,7 @@
 		<option value="cyan">CYAN</option>
 		<option value="pink">PINK</option>
 		<option value="night">NIGHT</option>
+		<option value="night-colorblind">NIGHT COLORBLIND</option>
 	</select>
 	<div class="felx w-full pt-2">
 		<div class="flex justify-center space-x-4">
