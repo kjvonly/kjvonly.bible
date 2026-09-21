06987f1fa1f74ffee06d2b47c2a70aed35f8ef1f
man4christ <man4christ.65qa8@simplelogin.com>
2026-09-20 20:16:02 -0400

    night color theme

--- client/kjvonly-pwa/src/lib/application/modules/settings/colorTheme.svelte
+++ client/kjvonly-pwa/src/lib/application/modules/settings/colorTheme.svelte
@@ -16,6 +16,7 @@
 		<option value="purple">PURPLE</option>
 		<option value="cyan">CYAN</option>
 		<option value="pink">PINK</option>
+		<option value="night">NIGHT</option>
 	</select>
 	<div class="felx w-full pt-2">
 		<div class="flex justify-center space-x-4">
